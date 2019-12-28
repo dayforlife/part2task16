9c012df57926ec1a2c876fdc29f086d1b852fc6a
dayforlife <59105746+dayforlife@users.noreply.github.com>
2019-12-28 20:17:46 +0600

    Delete task16.py

--- task16.py
+++
@@ -1,7 +0,0 @@
-moonMass = 60
-everyYear = 2 #pounds converted from kilo
-newYear = moonMass
-
-for x in range(1, 16):
-    newYear = newYear + everyYear 
-    print('My moon weight each year will be %s pounds on year %s' % (newYear, x))
